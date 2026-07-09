09c973992e5757c7abbfe774a9d6ac1ef21ee701
Amp <amp@amperture.com>
2026-07-09 17:34:38 -0500

    Orgmode

---
+++ lua/plugins/org.lua
@@ -0,0 +1,41 @@
+-- orgmode stuff
+
+return {
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/org/**/*",
+        org_default_notes_file = "~/org/refile.org",
+      })
+
+      -- Experimental LSP support
+      vim.lsp.enable("org")
+    end,
+  },
+  {
+    "chipsenkbeil/org-roam.nvim",
+    tag = "0.2.0",
+    dependencies = {
+      {
+        "nvim-orgmode/orgmode",
+        tag = "0.7.0",
+      },
+    },
+    config = function()
+      require("org-roam").setup({
+        directory = "~/org/roam",
+        --[[ optional
+        org_files = {
+          "~/another_org_dir",
+          "~/some/folder/*.org",
+          "~/a/single/org_file.org",
+        },
+        ]]
+      })
+    end,
+  },
+}
