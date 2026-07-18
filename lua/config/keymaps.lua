57b2e2e8fb6218934bfc57bef0b669eda68d26cd
Amp <amp@amperture.com>
2026-07-18 15:36:31 -0500

    Super Agenda

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Tmux Navigation
 vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
+
+-- Org SuperAgenda
+-- vim.keymap.set("n", "<leader>oa", ":OrgSuperAgenda<CR>", { silent = true })
+vim.keymap.set("n", "<leader>oA", ":OrgSuperAgenda!<CR>", { silent = true })
